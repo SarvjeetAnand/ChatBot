7a83ffed5d2d1c7771a0ede4e12a1983da29d379
SarvjeetAnand <sarvjeetkumaranand@gmail.com>
2025-01-20 20:00:45 +0530

    dburl

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+const urlDB = 'mysql://root:AeqtfMrjtGwyhEWJXvdyPGsRwKczzEay@mysql.railway.internal:3306/railway'
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  urlDB
 });
 
 
